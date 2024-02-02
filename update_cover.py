d0a7b7e0e9a6897cd8e14853c6725d187956c841
jason44 <90524351+jason44@users.noreply.github.com>
2024-02-01 16:29:06 -0800

    Add files via upload
    changed border detection method, and force square aspect ratio for majority of images except images with large background to foreground ratio

--- update_cover.py
+++ update_cover.py
@@ -5,7 +5,7 @@ import io
 from PIL import Image
 import argparse
 import eyed3
-
+import subprocess
 
 
 def extract_cover(mp3_path, out):
@@ -26,59 +26,123 @@ def crop_border(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     gray = cv2.equalizeHist(gray)
 
-    h, w, _ = image.shape
-    cv2.line(gray, (w//4, 0), ((w//2)+(w//4), 0), (255, 255, 255), 1)
-    cv2.line(gray, (w//4, h-1), ((w//2)+(w//4), h-1), (255, 255, 255), 1)
+    # thresholding is unnecessary after edge detection 
+    #_, gray = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
+    #gray = cv2.GaussianBlur(gray, (3, 3), 0)
 
-    gray = cv2.Canny(gray, 40, 80)
+    height, width, _ = image.shape
+    cv2.line(gray, (width//6, 4), (width-(width//6), 4), (255, 255, 255), 3)
+    cv2.line(gray, (width//6, height-4), (width-(width//6), height-4), (255, 255, 255), 3)
+
+    gray = cv2.Canny(gray, 40, 100, apertureSize=7, L2gradient=True)
+
     # Canny edge detection results in very thin lines. Particularly, 
 	# distinct lines aren't detected by findContours() so we dialate the lines 
     kernel = np.ones((2, 2),np.uint8)
     gray = cv2.morphologyEx(gray, cv2.MORPH_CLOSE, kernel, iterations = 2)
-
-    cv2.imshow("Test", gray)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-    # thresholding is unnecessary after edge detection 
-    #_, binary_mask = cv2.threshold(gray, avg/2, 255, cv2.THRESH_BINARY)
 
     # Find largest contour in the bitmap
     contours, _ = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     max_contour = max(contours, key=cv2.contourArea)
     x, y, w, h = cv2.boundingRect(max_contour)
 
+    #draw_bounds(gray, x, y, w, h)
+
     # ignore crop if it's too small 
-    if (w) < 300 or (h) < 300:
-        print("BOX TOO SMALL RETURNING ORIGINAL IMAGE")
+    if float(w) < float(width)/2.2 or float(h) < float(height)/2.2:
+        print("BOX TOO SMALL. RETURNING ORIGINAL IMAGE")
+        draw_bounds(gray, x, y, w, h)
+        if height * width < 679600:
+            return upscale(image)
         return image 
 
-    # send the cropped image in for a second pass (any more passes are generally redundant)
+    # unnecessary since we can just square images that are already square-like 
     cropped_gray = image[y:y+h, x:x+w]
     cropped_image = image[y:y+h, x:x+w]
+    #_, cropped_gray = cv2.threshold(cropped_gray, 5, 255, cv2.THRESH_BINARY)
     cropped_gray = cv2.cvtColor(cropped_gray, cv2.COLOR_BGR2GRAY)
-    cropped_gray = cv2.equalizeHist(cropped_gray)
-    cropped_gray = cv2.Canny(cropped_gray, 50, 100)
-    cropped_gray = cv2.morphologyEx(cropped_gray, cv2.MORPH_CLOSE, kernel, iterations = 1)
+    #cropped_gray = cv2.equalizeHist(cropped_gray)
+    cropped_gray = cv2.Canny(cropped_gray, 100, 40, apertureSize=3)
+    kernel = np.ones((2, 2),np.uint8)
+    cropped_gray = cv2.morphologyEx(cropped_gray, cv2.MORPH_CLOSE, kernel, iterations = 2)
 
     contours, _ = cv2.findContours(cropped_gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     max_contour = max(contours, key=cv2.contourArea)
     x, y, w, h = cv2.boundingRect(max_contour)
 
-    if (w) < 300 or (h) < 300:
-        print("BOX TOO SMALL RETURNING ORIGINAL IMAGE")
-        return image 
+    #draw_bounds(cropped_gray, x, y, w, h)
 
-    # 9 pixel border to compensate the dilation
-    cropped_image = cropped_image[y:y+h, x+9:x+w-9]
+    if float(w) < float(width)/2.2 or float(h) < float(height)/2.2:
+        print("BOX TOO SMALL. RETURNING ORIGINAL IMAGE (SECOND PASS).\n\
+              FALLING BACK TO FIRST PASS IMAGE")
+        #draw_bounds(cropped_gray, x, y, w, h)
+    else:
+        cropped_image = cropped_image[y:y+h, x:x+w]
+
+    # dilation was used so findContours can detect edges correctly,
+    # so, the boundedRect will be offsetted proportional to the dilation, 
+    # the solution is to square an image if it is already square-like
+    height, width, _ = cropped_image.shape
+    # this is always true lol
+    if float(width)/height < 1.15 or float(width)/height > 0.85:
+        print("SQUARING")
+        cropped_image = crop_to_square(cropped_image)
+
+    # use sr model for low quality images
+    height, width, _ = cropped_image.shape
+    if height * width < 679600:
+        print("UPSCALING")
+        return upscale(cropped_image)
 
     return cropped_image
+
+def crop_to_square(image):
+    height, width, _ = image.shape
+    min_dim = min(height, width)
+    startx = (height - min_dim) // 2
+    endx = startx + min_dim
+    # since all images are 16:9, we always have starty = 0
+    starty = (width - min_dim) // 2
+    endy = starty + min_dim
+
+    cropped_image = image[startx:endx, starty:endy]
+
+    return cropped_image
+
+def draw_bounds(image, x, y, w, h):
+    cv2.line(image, (x, y), (x+w, y), (255, 0, 0), 2)
+    cv2.line(image, (x, y), (x, y+h), (255, 0, 0), 2)
+    cv2.line(image, (x, y+h), (x+w, y+h), (255, 0, 0), 2)
+    cv2.line(image, (x+w, y), (x+w, y+h), (255, 0, 0), 2)
+
+    cv2.imshow("N", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def upscale(image):
+        # apply some noise before upscaling
+        height, width, _ = image.shape
+        cv2.imwrite("lowres_crop.jpg", image)
+        os.system(f"./realesrgan-ncnn-vulkan -i lowres_crop.jpg -o cropped_image.jpg -s 2")
+        cropped_img = Image.open("cropped_image.jpg")
+        # esrgan has really aggressive denoising, so we add some noise back into the upscaled image
+        arr = np.array(cropped_img)
+        avg = sum(cv2.mean(arr)) // 3.0
+        noise = np.random.normal(0, (height*width/(215**2)), arr.shape) 
+        noisy = np.clip(arr + noise, 0, 255).astype('uint8')
+        print(noise)
+        out = Image.fromarray(noisy)
+        out.save("cropped_image.jpg", 'JPEG', quality=75)
+        return None
 
 def cleanup():
     if os.path.exists("cover.jpg"):
         os.remove("cover.jpg")
     if os.path.exists("cropped_image.jpg"):
         os.remove("cropped_image.jpg")
+    if os.path.exists("lowres_crop.jpg"):
+        os.remove("lowres_crop.jpg")
 
 
 def embed_cover_art(mp3_path, cover_art_path):
@@ -115,7 +179,8 @@ def process_one(file):
             extract_cover(file, cover_path)
             image = cv2.imread(cover_path)
             cropped_image = crop_border(image)
-            cv2.imwrite("cropped_image.jpg", cropped_image)
+            if cropped_image is not None:
+                cv2.imwrite("cropped_image.jpg", cropped_image)
             embed_cover_art(file, "cropped_image.jpg")
             
 parser = argparse.ArgumentParser(description='Crops cover images of mp3 files')
@@ -128,7 +193,7 @@ if args.input:
     process_one(args.input)
     exit()
 if args.all:
-    process_all
+    process_all()
 if args.update:
     embed_cover_art(args.update, "cropped_image.jpg")
 
